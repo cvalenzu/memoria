d732214dbcfda36037e0da689517f61043889286
Camilo Valenzuela <cacke91@gmail.com>
2017-10-05 22:53:54 -0300

    Modified run_arima

--- cluster_scripts/run_arima.sh
+++ cluster_scripts/run_arima.sh
@@ -6,4 +6,15 @@
 #PBS -M camilo.valenzuela@alumnos.usm.cl
 #PBS -l walltime=64:00:00
 
-Rscript Memoria/code/r-code/arima.R
+#FILES
+#Memoria/data/processed/x_potency_canela1_merged.csv
+#Memoria/data/processed/x_potency_monte_redondo_merged.csv
+#Memoria/data/processed/x_potency_totoral_merged.csv
+
+for p in $(seq 0 5); do
+  for q in $(seq 0 1); do
+    for d in $(seq 0 5); do
+      Rscript Memoria/code/r-code/arima.R Memoria/data/processed/x_potency_canela1_merged.csv
+    done
+  done
+done
